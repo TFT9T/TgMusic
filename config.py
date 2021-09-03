5de7773f05b6a7c9894f265b276b11670c34ecaa
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2021-09-03 04:24:39 +0300

    Add Config file.

---
+++ config.py
@@ -0,0 +1,8 @@
+API_ID = 0  # API_ID from my.telegram.org.
+API_HASH = "HASH"  # my.telegram.org :)
+SUDO = 1088394097  # Your Telegam ID here.
+limits = {
+    "duration": 1200,  # Music duration limit in seconds, Default 1200 (20 min).
+    "calls": 10,  # Maximum number of active group call can be reached.
+    "playlist": 10,  # Playlist size limit per chat.
+}

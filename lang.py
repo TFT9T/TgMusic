4913d619ca46285b14ceaf8527949c4de079a07c
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2021-09-03 04:14:22 +0300

    Add English Language.

---
+++ lang.py
@@ -0,0 +1,71 @@
+# Here is bot texts.
+# feel free to fork the repository and add your language.
+# when you finish open issue.
+# Language: English
+lang = {
+    "download": "⏳",
+    "downloading": "Downloading {}%...",
+    "download_error": "Failed to download music.",
+    "downloaded": "Downloaded {}%.",
+    "joined": "Joined group call.",
+    "leving": "Am leving.",
+    "playing": "Playing",
+    "playlist": {
+        "empty": "Empty playlist.",
+        "playlist": "Playlist:",
+        "full": "Playlist is full.",
+        "exists": "This music already added in my playlist.",
+        "added": "Added to playlist.",
+        "only_one": "Playlist has only one music please play more.",
+    },
+    "help": """Commands:
+
+`/join` = Join group call.
+
+`/leave` = Leave group call.
+
+`/stop` = Stop music and delete all playlist.
+
+`/play` = Play replyed music.
+
+`/playlist` = Shows current chat playlist.
+
+`/skip` = Skips current music if playlist not empty.
+
+`/replay` = Replays current music.
+
+`/pause` = Pause current music.
+
+`/resume` = Resume current music.
+
+`/music` = :)
+
+`/mute` = Mute the bot, use `/pause` instead.
+
+`/ummute` = Unmute the bot.
+
+`/volume` = Sets bot volume 1 ~ 200, bot must be admin.
+""",
+    "mute": "Muted.",
+    "unmute": "Unmuted.",
+    "pause": "Paused.",
+    "stop": "Stoping everything, all data has been cleared.",
+    "resume": "Resumed.",
+    "replay": "Replaying ...",
+    "no_music": "Nothing played.",
+    "no_music_": "Play some music please :(",
+    "volume": "Volume {}%.",
+    "volume_warning": "From 1 to 200.",
+    "duration": "Duration:",
+    "duration_limit": "File too big, maximum of {} minutes allowed.",
+    "already_joined": "Already joined.",
+    "calls_limit": "Bot is full of calls retry later.",
+    "unsupported": "Cannot play this music.",
+    "skip": "Nothing to skip.",
+    "add_contact": "Added.",
+    "already_add_contact": "Already Added.",
+    "delete_contact": "Deleted.",
+    "already_delete_contact": "Not contact to delete.",
+    "total": "There is ( {} ) of active group Call.",
+    "no_total": "There is no active group call.",
+}

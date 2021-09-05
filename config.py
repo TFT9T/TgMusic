60eaaa3f92f4796b1fc6751d02c463bc62dd9c9c
AYMEN Mohammed <53928879+AYMENJD@users.noreply.github.com>
2021-09-05 07:35:54 +0300

    Add filesize limit

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ SUDO = 1088394097  # Your Telegam ID here.
 limits = {
     "duration": 1200,  # Music duration limit in seconds, Default 1200 (20 min).
     "calls": 10,  # Maximum number of active group call can be reached.
+    "filesize": 52428800,  # Maximum file size can be downloaded and played. In bytes, default 52428800 (50 mb).
     "playlist": 10,  # Playlist size limit per chat.
 }
